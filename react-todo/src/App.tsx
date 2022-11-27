706a7e6ab69fc41ef0356fb2ff9976c8d88c81a0
Swann HERRERA <swann@devloup.dev>
2022-11-27 11:06:31 +0100

    feat: add filter button

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -3,10 +3,26 @@ import './App.css';
 
 type InputEvent = React.KeyboardEvent<HTMLInputElement>;
 
+enum Filter {
+  ACTIVE = 'active',
+  ALL = 'all',
+  DONE = 'done',
+}
+
+type Todo = {
+  content: string;
+  isDone: boolean;
+}
+
 function App() {
   const inputRef = useRef(null);
-  const [todos, setTodos] = useState([] as string[]);
-  const addTodo = (todo: string) => {
+  const [activeButton, setActiveButton] = useState(Filter.ALL);
+  const [todos, setTodos] = useState([] as Todo[]);
+  const addTodo = (newTodoContent: string) => {
+    const todo = {
+      content: newTodoContent,
+      isDone: false,
+    };
     setTodos([...todos, todo]);
   };
   const onInputChange = (e: InputEvent) => {
@@ -15,14 +31,21 @@ function App() {
       addTodo(e.currentTarget.value);
     } 
   };
+
   return (
     <div className="App">
       <main>
         <div className='modal'>
           <input ref={inputRef} type="text" onKeyUp={onInputChange} />
+          <div>{activeButton}</div>
           <ol>
-            {todos.map((todo) => <li>{todo}</li>)}
+            {todos.map((todo) => <li>{todo.content}</li>)}
           </ol>
+          <div className="button-list">
+            <button onClick={() => setActiveButton(Filter.ACTIVE)} type='button' className={activeButton === "active" ? 'active' : ""}>Active</button>
+            <button  onClick={() => setActiveButton(Filter.DONE)} type='button' className={activeButton === "done" ? 'active' : ""}>Done</button>
+            <button  onClick={() => setActiveButton(Filter.ALL)} type='button' className={activeButton === "all" ? 'active' : ""}>All</button>
+          </div>
         </div>
       </main>
     </div>
